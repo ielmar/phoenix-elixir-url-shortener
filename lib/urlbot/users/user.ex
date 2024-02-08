f2ae958693eff4f0dc3f501840a7e2d7bb10d2f1
Elmar Ismayilov <bamazy@gmail.com>
2024-02-08 15:47:47 +0800

    install pow

---
+++ lib/urlbot/users/user.ex
@@ -0,0 +1,10 @@
+defmodule Urlbot.Users.User do
+  use Ecto.Schema
+  use Pow.Ecto.Schema
+
+  schema "users" do
+    pow_user_fields()
+
+    timestamps()
+  end
+end
--- lib/urlbot_web/endpoint.ex
+++ lib/urlbot_web/endpoint.ex
@@ -47,5 +47,6 @@ defmodule UrlbotWeb.Endpoint do
   plug Plug.MethodOverride
   plug Plug.Head
   plug Plug.Session, @session_options
+  plug Pow.Plug.Session, otp_app: :urlbot
   plug UrlbotWeb.Router
 end
